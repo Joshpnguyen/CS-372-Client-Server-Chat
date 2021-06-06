622d785fa557e70b3bc2eaa6d20317bdbbe7bdd4
Josh Nguyen <joshpnguyen@gmail.com>
2021-06-05 19:49:40 -0700

    Client and server connection completed. Client sending message to server complete. Server needs to send message and intro.

--- client.py
+++ client.py
@@ -5,15 +5,19 @@ client = socket(AF_INET, SOCK_STREAM)
 client_IP = "localhost"
 client_port = int(input("Input Port Number: "))
 
+############################
+# Code structure below cited from:
+# https://realpython.com/python-sockets/
+############################
+
 with client as c:
     c.connect((client_IP, client_port))
     msg = ""
 
     while msg != "/q":
         received = c.recv(2048)
-        msg = input(">")
+        print(received.decode())
+        msg = input("> ")
         c.sendall(str.encode(msg))
 
-
-
-print("Closing client.")
+print("*** Closing client ***")
--- server.py
+++ server.py
@@ -6,6 +6,10 @@ server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 server_IP = "localhost"
 server_port = int(input("Input Port Number: "))
 
+############################
+# Code structure below cited from:
+# https://realpython.com/python-sockets/
+############################
 
 with server as s:
     s.bind((server_IP, server_port))
@@ -18,11 +22,14 @@ with server as s:
     with conn:
         print("Connected by " + str(addr))
         print("Waiting for message from client..")
-        conn.sendall(b"Type /q to quit")
-        conn.sendall(b"Enter message to send..")
+        conn.sendall(b"Type /q to quit\nEnter message to send..")
 
         while 1:
-            msg = conn.recv(2048)
-            if not msg:
+            recv = conn.recv(2048)
+            if not recv or recv.decode() == "/q":
                 break
-            print(msg.decode())
+            print(recv.decode())
+            msg = input("> ")
+            conn.sendall(str.encode(msg))
+
+print("*** Closing server ***")
