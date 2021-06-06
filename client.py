45df80a543791176f1f6db0cda3d3fe5d62095d4
Josh Nguyen <joshpnguyen@gmail.com>
2021-06-05 17:16:43 -0700

    Client and server connection completed. Client sending message to server complete. Server needs to send message and intro.

--- client.py
+++ client.py
@@ -1,2 +1,19 @@
 from socket import *
 
+client = socket(AF_INET, SOCK_STREAM)
+
+client_IP = "localhost"
+client_port = int(input("Input Port Number: "))
+
+with client as c:
+    c.connect((client_IP, client_port))
+    msg = ""
+
+    while msg != "/q":
+        received = c.recv(2048)
+        msg = input(">")
+        c.sendall(str.encode(msg))
+
+
+
+print("Closing client.")
--- server.py
+++ server.py
@@ -3,3 +3,26 @@ from socket import *
 server = socket(AF_INET, SOCK_STREAM)
 server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
+server_IP = "localhost"
+server_port = int(input("Input Port Number: "))
+
+
+with server as s:
+    s.bind((server_IP, server_port))
+    s.listen(1)  # listen for 1 connection
+
+    print("Server listening on", server_IP, "on port", server_port)
+
+    conn, addr = s.accept()
+
+    with conn:
+        print("Connected by " + str(addr))
+        print("Waiting for message from client..")
+        conn.sendall(b"Type /q to quit")
+        conn.sendall(b"Enter message to send..")
+
+        while 1:
+            msg = conn.recv(2048)
+            if not msg:
+                break
+            print(msg.decode())
